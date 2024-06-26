e00d97e3c0e3e95a984a2c9519a5b67f4d325365
guilhermeramosbr <ramosguilherme175@gmail.com>
2024-06-25 21:27:23 -0300

    versao 1 trabalho

---
+++ script.js
@@ -0,0 +1,51 @@
+let personagens = []
+const div = document.querySelector('#lista_personagem')
+function salvarProtagonista(event){
+    const dadosPersonagem = []
+    event.preventDefault()
+    const form = document.querySelector('form')
+    const input = new FormData(form)
+    
+
+    const foto = input.get('foto')
+    const heroi = input.get('heroi')
+    const filme = input.get('filme')
+    const ator = input.get('ator')
+    const descricao = input.get('descricao')
+    const habilidade = input.get('habilidade')
+    dadosPersonagem.push(foto,heroi,filme,ator,descricao,habilidade)
+    personagens.push(dadosPersonagem)
+
+    localStorage.setItem('personagens', JSON.stringify(personagens))
+
+
+    div.innerHTML += `<div>
+    <img src="${dadosPersonagem[0]} " alt="">
+    <h2>${dadosPersonagem[1]}</h2>
+    <p>${dadosPersonagem[2]}</p>
+    <p>${dadosPersonagem[3]}</p>
+    <p>${dadosPersonagem[4]}</p>
+    <p>${dadosPersonagem[5]}</p>
+  </div>`
+
+
+}
+let personagensArmazenados = localStorage.getItem('personagens')
+if(personagensArmazenados){
+    personagens =  JSON.parse(personagensArmazenados)
+  
+    for(const dadosPersonagem of personagens){
+        div.innerHTML += `<div>
+    <img src="${dadosPersonagem[0]} " alt="">
+    <h2>${dadosPersonagem[1]}</h2>
+    <p>${dadosPersonagem[2]}</p>
+    <p>${dadosPersonagem[3]}</p>
+    <p>${dadosPersonagem[4]}</p>
+    <p>${dadosPersonagem[5]}</p>
+  </div>`
+
+    }
+
+
+}
+
